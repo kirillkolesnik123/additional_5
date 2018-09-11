1c5c97a76c1afa0d5ac2c03bed7cd43c53ab2972
kirillkolesnik123 <42231774+kirillkolesnik123@users.noreply.github.com>
2018-09-12 00:16:22 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+  let stack = [];
+  str = str.split('');
+
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] == bracketsConfig[j][1] && stack[stack.length - 1] == bracketsConfig[j][0]) {
+        stack.pop();
+      } else if (str[i] === bracketsConfig[j][0]) {
+        stack.push(str[i]);
+      }
+    }
+  }
+
+  if (stack.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
